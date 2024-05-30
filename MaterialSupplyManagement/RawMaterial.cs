561233fed4e25cceaadf58559beea74e507650c4
Andrii <Andrey1w2005@gmail.com>
2024-05-31 02:34:07 +0300

    .

--- MaterialSupplyManagement/RawMaterial.cs
+++ MaterialSupplyManagement/RawMaterial.cs
@@ -2,66 +2,65 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using MaterialSupplyManagement.DAL;
 
-namespace MaterialSupplyManagement
+namespace MaterialSupplyManagement;
+
+public class RawMaterial : Material
 {
-    public class RawMaterial : Material
+    private double _pricePerKg;
+    private int _weightTons;
+
+    [Column(ColumnNames.TotalPrice)]
+    public double TotalPrice { get; set; }
+
+    [Column(ColumnNames.PricePerKg)]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a valid price per kilogram")]
+    [Required(ErrorMessage = "Price per kilogram is required")]
+    public double PricePerKg
     {
-        private double _pricePerKg;
-        private int _weightTons;
-
-        [Column(ColumnNames.TotalPrice)]
-        public double TotalPrice { get; set; }
-
-        [Column(ColumnNames.PricePerKg)]
-        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a valid price per kilogram")]
-        [Required(ErrorMessage = "Price per kilogram is required")]
-        public double PricePerKg
+        get => _pricePerKg;
+        set
         {
-            get => _pricePerKg;
-            set
-            {
-                _pricePerKg = value;
-                UpdateLastModified();
-                UpdateTotalPrice();
-            }
-        }
-
-        [Column(ColumnNames.WeightTons)]
-        [Range(0, int.MaxValue, ErrorMessage = "Please enter a valid weight in tons")]
-        [Required(ErrorMessage = "Weight in tons is required")]
-        public int WeightTons
-        {
-            get => _weightTons;
-            set
-            {
-                _weightTons = value;
-                UpdateLastModified();
-                UpdateTotalPrice();
-            }
-        }
-
-        public RawMaterial()
-        {
+            _pricePerKg = value;
             UpdateLastModified();
             UpdateTotalPrice();
         }
-
-        public RawMaterial(string name, string type, string description, double pricePerKg, int weightTons)
-            : base(name, type, description)
-        {
-            PricePerKg = pricePerKg;
-            WeightTons = weightTons;
-
-            UpdateLastModified();
-            UpdateTotalPrice();
-        }
-
-        private void UpdateTotalPrice()
-        {
-            TotalPrice = PricePerKg * WeightTons * 1000;
-        }
-
-        public override string GetInfoString() =>
-            $"Raw Material: {Name}, Price per kilogram: {PricePerKg}$, Total weight in tons: {WeightTons}, Total price: {TotalPrice}$\n\nAdditional info:\n{Description}";
     }
+
+    [Column(ColumnNames.WeightTons)]
+    [Range(0, int.MaxValue, ErrorMessage = "Please enter a valid weight in tons")]
+    [Required(ErrorMessage = "Weight in tons is required")]
+    public int WeightTons
+    {
+        get => _weightTons;
+        set
+        {
+            _weightTons = value;
+            UpdateLastModified();
+            UpdateTotalPrice();
+        }
+    }
+
+    public RawMaterial()
+    {
+        UpdateLastModified();
+        UpdateTotalPrice();
+    }
+
+    public RawMaterial(string name, string type, string description, double pricePerKg, int weightTons)
+        : base(name, type, description)
+    {
+        PricePerKg = pricePerKg;
+        WeightTons = weightTons;
+
+        UpdateLastModified();
+        UpdateTotalPrice();
+    }
+
+    private void UpdateTotalPrice()
+    {
+        TotalPrice = PricePerKg * WeightTons * 1000;
+    }
+
+    public override string GetInfoString() =>
+        $"Raw Material: {Name}, Price per kilogram: {PricePerKg}$, Total weight in tons: {WeightTons}, Total price: {TotalPrice}$\n\nAdditional info:\n{Description}";
 }
--- MaterialSupplyManagement/obj/Debug/net8.0/MaterialSupplyManagement.AssemblyInfo.cs
+++ MaterialSupplyManagement/obj/Debug/net8.0/MaterialSupplyManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaterialSupplyManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc3d504d2ef4ffab390d6eac4337ccd87f1a571e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5aa89493d3f944bc6960362e87a2002dd9ca57b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaterialSupplyManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaterialSupplyManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
